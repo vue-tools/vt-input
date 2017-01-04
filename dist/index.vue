49178c20172e5fc8285ae5b8ddcfaabcbbe029b6
2046 <g@ggg.gd>
2017-01-05 03:08:28 +0800

    init

---
+++ dist/index.vue
@@ -0,0 +1,45 @@
+<style src="./style.css"></style>
+<template src="./template.html"></template>
+
+<script>
+    export default {
+        props: {
+            type: {
+                type: String,
+                default: 'text'
+            },
+            size: String,
+            form: String,
+            name: String,
+            disabled: Boolean,
+            readonly: Boolean,
+            autofocus: Boolean,
+            placeholder: String,
+            max: [String, Number],
+            min: [String, Number],
+            value: [String, Number],
+            maxlength: [String, Number],
+            minlength: [String, Number]
+        },
+        computed: {
+            clazz() {
+                return {
+                    'ui-input--mini': this.size === 'mini',
+                    'ui-input--large': this.size === 'large'
+                }
+            }
+        },
+        methods: {
+            blurHandler(e) {
+                this.$emit('blur', e)
+            },
+            focusHandler(e) {
+                this.$emit('focus', e)
+            },
+            inputHandler(e) {
+                this.$emit('input', e.target.value)
+                this.$emit('change', e.target.value)
+            }
+        }
+    }
+</script>
